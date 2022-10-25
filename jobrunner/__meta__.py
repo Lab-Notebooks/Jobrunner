f10a0fc76a3f4e30879ede04af7f6bc61877f4c1
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2022-10-24 23:48:22 -0500

    update version for jobrunner

--- jobrunner/__meta__.py
+++ jobrunner/__meta__.py
@@ -1,7 +1,7 @@
 """Metadata for jobrunner"""
 
 __pkgname__ = "PyJobRunner"
-__version__ = "1.9"
+__version__ = "2.0"
 __authors__ = "Akash Dhruv"
 __license__ = "MIT License"
 __copyright__ = "Copyright (c) Akash Dhruv 2022. All Rights Reserved."
