2d364b008555a006a2de82ccfa6f8de7f0e6ad1a
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-06-21 19:33:35 -0500

    update reference solutions

--- tests/SimpleProject/JobObject/setupSolution.sh
+++ tests/SimpleProject/JobObject/setupSolution.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-JobWorkDir="/home/runner/work/Jobrunner/tests/SimpleProject/JobObject"
+JobWorkDir="/home/runner/work/Jobrunner/Jobrunner/tests/SimpleProject/JobObject"
 
-cd /home/runner/work/Jobrunner/tests/SimpleProject
+cd /home/runner/work/Jobrunner/Jobrunner/tests/SimpleProject
 
 # Bash file for environment
 
@@ -10,7 +10,7 @@ echo "Hello from environment script"
 export ENV_VAR_1="First environment variable"
 Env_Var_2="Second environment variable"
 
-cd /home/runner/work/Jobrunner/tests/SimpleProject/JobObject
+cd /home/runner/work/Jobrunner/Jobrunner/tests/SimpleProject/JobObject
 
 # setup script
 
